887d4dc18854f2908e51d3a538161eb11f554a62
Dima Leonov <d.leonov@m2e.team>
2022-08-23 10:10:28 +0300

    Add some changes

--- test_product_page.py
+++ test_product_page.py
@@ -15,15 +15,6 @@ class TestUserAddToBasketFromProductPage:
         page.register_new_user()
         page.should_be_authorized_user()
 
-    def test_guest_can_add_product_to_basket(self, browser):
-        link = f"http://selenium1py.pythonanywhere.com/uk/catalogue/coders-at-work_207/?promo=offer2"
-        add_product = ProductPage(browser, link)
-        add_product.open()
-        add_product.add_product_to_basket()
-        add_product.solve_quiz_and_get_code()
-        add_product.product_added_to_basket_message()
-        add_product.product_price_added_to_basket()
-
     def test_user_can_add_product_to_basket(self, browser):
         link = f"http://selenium1py.pythonanywhere.com/uk/catalogue/coders-at-work_207/?promo=offer2"
         add_product = ProductPage(browser, link)
@@ -33,21 +24,36 @@ class TestUserAddToBasketFromProductPage:
         add_product.product_added_to_basket_message()
         add_product.product_price_added_to_basket()
 
-    def test_guest_can_go_to_login_page_from_product_page(self, browser):
-        link = f"http://selenium1py.pythonanywhere.com/uk/catalogue/coders-at-work_207/?promo=offer0"
-        product_page = ProductPage(browser, link)
-        product_page.open()
-        product_page.should_be_login_link()
-        product_page.go_to_login_page()
 
-    def test_guest_cant_see_product_in_basket_opened_from_product_page(self, browser):
-        link = "http://selenium1py.pythonanywhere.com/uk/catalogue/growing-object-oriented-software-guided-by-tests_123/"
-        page = BasketPage(browser, link)
-        page.open()
-        page.should_be_basket_link()
-        page.go_to_basket()
-        page.should_not_be_products_in_empty_basket()
-        page.should_be_message_about_empty_basket()
+@pytest.mark.need_review
+def test_guest_can_add_product_to_basket(browser):
+    link = f"http://selenium1py.pythonanywhere.com/uk/catalogue/coders-at-work_207/?promo=offer2"
+    add_product = ProductPage(browser, link)
+    add_product.open()
+    add_product.add_product_to_basket()
+    add_product.solve_quiz_and_get_code()
+    add_product.product_added_to_basket_message()
+    add_product.product_price_added_to_basket()
+
+
+@pytest.mark.need_review
+def test_guest_can_go_to_login_page_from_product_page(browser):
+    link = f"http://selenium1py.pythonanywhere.com/uk/catalogue/coders-at-work_207/?promo=offer0"
+    product_page = ProductPage(browser, link)
+    product_page.open()
+    product_page.should_be_login_link()
+    product_page.go_to_login_page()
+
+
+@pytest.mark.need_review
+def test_guest_cant_see_product_in_basket_opened_from_product_page(browser):
+    link = "http://selenium1py.pythonanywhere.com/uk/catalogue/growing-object-oriented-software-guided-by-tests_123/"
+    page = BasketPage(browser, link)
+    page.open()
+    page.should_be_basket_link()
+    page.go_to_basket()
+    page.should_not_be_products_in_empty_basket()
+    page.should_be_message_about_empty_basket()
 
     # def test_user_cant_see_success_message(self, browser):
     #     link = f"http://selenium1py.pythonanywhere.com/uk/catalogue/coders-at-work_207/?promo=offer0"
